dab1f9134c33a222c35a3190f111259da6042419
hatfield-c <chatfield@ymail.com>
2017-11-28 00:36:11 -0600

    [PRJ-19] Compile a list of valid moves for 'X' and 'O' players

--- GameBoard.asm
+++ GameBoard.asm
@@ -7,10 +7,8 @@
 	ptr_a1:			.word		0
 	ptr_a2:			.word		0
 	ptr_a3:			.word		0
+	ptr_a4:			.word 		0
 	
-	xMovesList:		.space		256
-	oMovesList:		.space		256
-
 .text
 .globl readBoardPosition
 readBoardPosition:
@@ -145,7 +143,6 @@ writeElement:
 	#method: Return to callee
 	jr $ra 
 
-.globl getElement
 getElement:
 	# $a0 : Row of table
 	# $a1 : Column of table
@@ -172,20 +169,244 @@ getElement:
 	
 	#method: Return to callee
 	jr $ra
+	
+isEmpty:
+	# $v0 : if the board position is empty
+	# $a0 : row of board
+	# $a1 : column of board
 
-.globl isPositionValidMove
-isPositionValidMove:
+	#method: Place arguments in memory
+	sw $a0, ptr_a0
+	sw $a1, ptr_a1
+	
+	#method: Save registers to the stack
+	move $a0, $ra
+	jal saveReturnAdd
+	
+	#method: Read the ascii code at the board position
+	lw $a0, ptr_a0
+	lw $a1, ptr_a1
+	jal readBoardPosition
+	move $t0, $v0
+	
+	#condition: If the space is empty, return true
+	li $v0, 0
+	bne $t0, $v0, non_empty
+	li $v0, 1
+	non_empty:
+	
+	jal loadReturnAdd
+	jr $ra
+	
+canCaptureDirection:
+	# $a0 : initial row
+	# $a1 : initial column
+	# $a2 : row direction
+	# $a3 : column direction
+	# $s4 : player piece code
+	
+	#method: Place arguments in memory
+	sw $a0, ptr_a0
+	sw $a1, ptr_a1
+	sw $a2, ptr_a2
+	sw $a3, ptr_a3
+	
+	#method: Save registers to the stack
+	move $a0, $ra
+	jal saveAllRegisters
+	
+	#method: Pull arguments from memory
+	lw $s0, ptr_a0
+	lw $s1, ptr_a1
+	lw $s2, ptr_a2
+	lw $s3, ptr_a3
+	lw $s4, ptr_a4
+	
+	# $t4 : current row
+	# $t5 : current column
+	# $t6 : current board position value
+	# $t7 : is a valid direction
+	move $t4, $s0
+	move $t5, $s1
+	li $t6, 0
+	li $t7, 0
+	
+	#method: Claculate the first position to check
+	add $t4, $t4, $s2
+	add $t5, $t5, $s3
+	
+	#condition: If the offset is not within the bounds of the table, then the direction is invalid
+	blt $t4, 0, not_valid_direction
+	blt $t5, 0, not_valid_direction
+	bgt $t4, 7, not_valid_direction
+	bgt $t5, 7, not_valid_direction
+	
+	#method: Get the current space value to check
+	move $a0, $t4
+	move $a1, $t5
+	jal readBoardPosition
+	move $t6, $v0
+	
+	#method: If the first offset position is empty or has the current players piece, then the direction is invalid
+	beqz $t6, not_valid_direction
+	beq $t6, $s4, not_valid_direction
+	
+	#method: Check each successive position
+	check_next_position:
+	
+		#method: Claculate the next position to check
+		add $t4, $t4, $s2
+		add $t5, $t5, $s3
+	
+		#condition: If the offset is not within the bounds of the table, then the direction is invalid
+		blt $t4, 0, not_valid_direction
+		blt $t5, 0, not_valid_direction
+		bgt $t4, 7, not_valid_direction
+		bgt $t5, 7, not_valid_direction
+		
+		#method: Get the next space value to check
+		move $a0, $t4
+		move $a1, $t5
+		jal readBoardPosition
+		move $t6, $v0
+		
+		#condition: If the next space is empty, exit with false.
+		#	    If the next space has an enemy piece, check the next space
+		#	    If the next space has friendly piece, the move is valid
+		beqz $t6, not_valid_direction
+		bne $t6, $s4, check_next_position
+		li $t7, 1
+	
+	not_valid_direction:
+	
+	#method: Move the final result to the return register
+	move $v0, $t7
+	
+	#method: Reload all registers, and return to caller
+	jal loadAllRegisters
+	jr $ra
+
+.globl isValidMove
+isValidMove:
 	# $v0 : If the move is valid
 	# $a0 : Row of board
 	# $a1 : Column of board
+	# $a2 : Player piece code
+	# $s3 : Result of check
 	
+	#method: Place arguments in memory
+	sw $a0, ptr_a0
+	sw $a1, ptr_a1
+	sw $a2, ptr_a2
 	
+	#method: Save registers to the stack
+	move $a0, $ra
+	jal saveAllRegisters
 	
-	jr $ra
+	#method: Pull arguments from memory
+	lw $s0, ptr_a0
+	lw $s1, ptr_a1
+	lw $s2, ptr_a2
+	li $s3, 0
+	
+	#method: Check if space is empty
+	move $a0, $s0
+	move $a1, $s1
+	jal isEmpty
+	beqz $v0, not_valid_move
 
-.globl compileValidMoves
-compileValidMoves:
-
-	# $a0 : Ascii code of player to check
-
+	#method: Check if this move can capture a piece in any of eight directions
+	
+	# top left
+	move $a0, $s0
+	move $a1, $s1
+	li $a2, -1
+	li $a3, -1
+	li $t0, 5
+	sw $t0, ptr_a4
+	jal canCaptureDirection
+	beq $v0, 1, valid_move
+	
+	# top center
+	move $a0, $s0
+	move $a1, $s1
+	li $a2, -1
+	li $a3, 0
+	li $t0, 5
+	sw $t0, ptr_a4
+	jal canCaptureDirection
+	beq $v0, 1, valid_move
+	
+	# top right
+	move $a0, $s0
+	move $a1, $s1
+	li $a2, -1
+	li $a3, 1
+	li $t0, 5
+	sw $t0, ptr_a4
+	jal canCaptureDirection
+	beq $v0, 1, valid_move
+	
+	# middle left
+	move $a0, $s0
+	move $a1, $s1
+	li $a2, 0
+	li $a3, -1
+	li $t0, 5
+	sw $t0, ptr_a4
+	jal canCaptureDirection
+	beq $v0, 1, valid_move
+	
+	# middle right
+	move $a0, $s0
+	move $a1, $s1
+	li $a2, 0
+	li $a3, 1
+	li $t0, 5
+	sw $t0, ptr_a4
+	jal canCaptureDirection
+	beq $v0, 1, valid_move
+	
+	# bottom left
+	move $a0, $s0
+	move $a1, $s1
+	li $a2, 1
+	li $a3, -1
+	li $t0, 5
+	sw $t0, ptr_a4
+	jal canCaptureDirection
+	beq $v0, 1, valid_move
+	
+	# bottom center
+	move $a0, $s0
+	move $a1, $s1
+	li $a2, 1
+	li $a3, 0
+	li $t0, 5
+	sw $t0, ptr_a4
+	jal canCaptureDirection
+	beq $v0, 1, valid_move
+	
+	# bottom right
+	move $a0, $s0
+	move $a1, $s1
+	li $a2, 1
+	li $a3, 1
+	li $t0, 5
+	sw $t0, ptr_a4
+	jal canCaptureDirection
+	beq $v0, 1, valid_move
+	
+	#condition: If no captures in any direction, move is not valid
+	j not_valid_move
+	
+	#method: If the direction is valid, return true
+	valid_move:
+	li $s3, 1
+		
+	not_valid_move:
+	move $v0, $s3
+	
+	#method: Reload all registers, and return to caller
+	jal loadAllRegisters
 	jr $ra
--- Reversi.asm
+++ Reversi.asm
@@ -11,21 +11,52 @@ main:
 	li $a2, 0
 	jal writeBoardPosition
 	
-	li $a0, 0
+	li $a0, 5
 	li $a1, 0
-	jal readBoardPosition
-	move $s1, $v0
-	
-	li $a0, 6
-	li $a1, 7
-	li $a2, 7
+	li $a2, 1
+	jal writeBoardPosition
+#	
+#	li $a0, 0
+#	li $a1, 0
+#	jal readBoardPosition
+#	move $s1, $v0
+#	
+	li $a0, 5
+	li $a1, 1
+	li $a2, 1
 	jal writeBoardPosition
 	
-	li $a0, 7
-	li $a1, 7
-	jal readBoardPosition
-	move $s2, $v0
-
+	li $a0, 5
+	li $a1, 2
+	li $a2, 1
+	jal writeBoardPosition
+	
+	li $a0, 3
+	li $a1, 1
+	li $a2, 5
+	jal isValidMove
+	move $s1, $v0
+	
+#	
+#	li $a0, 7
+#	li $a1, 7
+#	jal readBoardPosition
+#	move $s2, $v0
+#	
+#	li $a0, 0
+#	li $a1, 0
+#	jal isValidMove
+#	move $s3, $v0
+	
+#	li $a0, 0
+#	li $a1, 1
+#	jal isValidMove
+#	move $s4, $v0
+#	
+#	li $a0, 7
+#	li $a1, 7
+#	jal isValidMove
+#	move $s4, $v0
 	
 	#method: End the application
 	li $v0, 10
